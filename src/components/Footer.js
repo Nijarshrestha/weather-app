d1bf05fb2914b8d3b5e1e077e7b1182497288c5f
Nijar Shrestha <nijar@bottle.com.np>
2020-05-13 10:52:20 +0545

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        {" "}
+        Designed and developed by Nijar Shrestha (a.k.a Sevack) with the help
+        from Hamz.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Tagline.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Tagline = () => (
+  <div className="tagline">
+    <p>Enter the name of the city to get started</p>
+  </div>
+);
